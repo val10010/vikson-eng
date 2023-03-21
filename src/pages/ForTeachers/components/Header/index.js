7fc3f23ab4a87efb25cbe5354a17720d58b30e16
mrolesha <116891703+mrolesha@users.noreply.github.com>
2023-03-21 19:56:56 +0200

    Olega chtoto izmenil

--- src/pages/ForTeachers/components/Header/index.js
+++ src/pages/ForTeachers/components/Header/index.js
@@ -12,7 +12,6 @@ const Header = () => {
                 <Link to='#'>Вакансії</Link>
             </Menu>
 
-
         </header>
     );
 };
--- src/pages/ForTeachers/index.js
+++ src/pages/ForTeachers/index.js
@@ -5,6 +5,7 @@ const ForTeachers = () => {
     return (
         <>
             <Header/>
+
         </>
     );
 };
