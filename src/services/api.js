c7dde476c165d904a9761b9d0d93e125a23d2d95
v.kiriiak <valentyn.kiriiak@playtech.com>
2024-03-28 13:48:45 +0200

    added

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Default instance
 export const instance = axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:9000' : 'https://api-routes-rest-git-main-val10010.vercel.app',
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:9000' : 'https://api-routes-rest-git-main-val10010s-projects.vercel.app',
     responseType: 'json',
     headers: {
         'Content-Type': 'application/json'
